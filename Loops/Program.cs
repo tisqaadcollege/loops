e2587baa39ee80117b9e410d09b2141af94e8317
halac <halac122@gmail.com>
2022-05-29 19:27:03 +0300

    Added Do while

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -16,7 +16,7 @@ namespace Loops
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmForLoop());
+            Application.Run(new frmdoWhileLopp());
         }
     }
 }
---
+++ Loops/frmdoWhileLopp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loops
+{
+    public partial class frmdoWhileLopp : Form
+    {
+        public frmdoWhileLopp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            // DO WHILE LOOP 
+
+            int number = 1000;
+
+            do
+            {
+                lstOutput.Items.Add(number);
+                number++;
+            } while (number <= 10);
+        }
+    }
+}
